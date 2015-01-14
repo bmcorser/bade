e8076d14b623258a37fbddec9f2e4b61c33977a5
bmcorser <bmcorser@gmail.com>
2015-01-14 06:59:31 +0000

    Fix for fresh build, bump

--- bade/directives/dotgraph.py
+++ bade/directives/dotgraph.py
@@ -1,3 +1,4 @@
+import os
 from docutils import nodes
 from dot_parser import parse_dot_data
 
@@ -6,8 +7,11 @@ def closed_dotgraphdirective(build):
     'Close the directive with the build directory'
     def DotgraphDirective(name, arguments, options, content, *args):
         graph = parse_dot_data('\n'.join(content))
-        outpath = arguments[0]
-        graph.write_svg(build + outpath)
+        outpath = build + arguments[0]
+        basedir = os.path.dirname(outpath)
+        if not os.path.exists(basedir):
+            os.makedirs(basedir)
+        graph.write_svg(outpath)
         html = '<img src="{0}" class="dot-graph" />'.format(outpath)
         return [nodes.raw('', html, format='html')]
     DotgraphDirective.arguments = (1, 0, 1)
