6f23840114306fccbc41411716762999db19c0e2
bmcorser <bmcorser@gmail.com>
2015-02-09 16:02:33 +0000

    Bamp from where?

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readme():
 
 setup(
     name=PKG_NAME,
-    version='0.1.6',
+    version='0.1.7',
     description='Micro-blogging with rST',
     data_files=include('templates'),
     packages=find_packages(),
