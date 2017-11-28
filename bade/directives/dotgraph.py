8a1cabf66ee149275fd4c1ff4f9eb48336029be9
Ben Corser <ben.corser@ly.st>
2017-11-28 11:08:43 +0000

    Upstream commit at bmcorser/blog@3c13a3453fa7bf22b41f975e7b38d8e9ea5ec495

--- bade/directives/dotgraph.py
+++ bade/directives/dotgraph.py
@@ -9,7 +9,7 @@ def dotgraph_directive(name, arguments, options, content, *args):
     graph = pydot.dot_parser.parse_dot_data('\n'.join(content))[0]
     tmp = tempfile.NamedTemporaryFile()
     print("Graph temp: {0}".format(tmp.name))
-    graph.write_svg(tmp.name)
+    graph[0].write_svg(tmp.name)
     svg = BeautifulSoup(tmp.read().decode('utf8'), 'html.parser')
     svg.find('svg').attrs['class'] = 'dot-graph'
     return [nodes.raw('', str(svg), format='html')]
